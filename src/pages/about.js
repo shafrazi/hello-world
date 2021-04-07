a2ff0a93d68a56adcaa4da254659a31d9865cbfb
shafrazi <shafrazi@live.com>
2021-04-07 23:40:41 +0530

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+  return (
+    <div>
+      <Header headerText={"About"} />
+      <h1>About Gatsby</h1>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Header headerText={"hello!"} />
+      <Link to="/contact">Contact</Link>
+      <h1>hello world!</h1>
+      <p>Helloooo</p>
+    </div>
+  )
 }
